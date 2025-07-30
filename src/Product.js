4aba9fa665d141d5f3ed58850a862ff5cc1152f3
sindhu <muchalla.sindhura@gmail.com>
2025-07-29 22:05:54 -0500

    deleted unwanted data

---
+++ src/Product.js
@@ -0,0 +1,14 @@
+
+import './Product.css';
+import React, { useState } from 'react';
+
+const ProductListing = () =>{
+  return (
+    <div className='container'>
+
+    </div>
+    
+  );
+}
+
+export default ProductListing;
